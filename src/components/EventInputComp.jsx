180ffc2500968cbbedeb2cf7259a6802e9e91f5a
Ethan Hasman <hasman1982@gmail.com>
2020-05-09 15:35:25 +0300

    Creating Compponents Files

---
+++ src/components/EventInputComp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../components/CountDownComp.css'
+
+function EventInputComp() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default EventInputComp
